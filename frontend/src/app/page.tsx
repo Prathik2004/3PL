abdcc21981ebee3b14db35ba792c15682232df95
Jasskaran-Singh-Shangari <jasskaransingh0007@gmail.com>
2026-02-23 17:59:38 +0530

    Bulk Upload Modal added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
+import BasicInput from "../components/ui/BasicInput";
 import DeleteModal from "../components/ui/DeleteModal";
 
 export default function Home() {
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen">
 
-      <DeleteModal />
+      <BasicInput text="SHIPMENT ID" />
     </h1>
   );
 }
