012fd1fceaef213fe15591c4658922937485571c
Jasskaran-Singh-Shangari <jasskaransingh0007@gmail.com>
2026-02-23 18:22:32 +0530

    Minor chnages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,6 @@ import DeleteModal from "../components/ui/DeleteModal";
 export default function Home() {
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen">
-
-      <BasicInput text="SHIPMENT ID" />
     </h1>
   );
 }
