821c26c9842ae3752927170ffd615ced49101e5a
Samrat Dutta <trexgodmode777@gmail.com>
2026-02-23 18:08:33 +0530

    fix: required placeholder missing on BasicInput causing build fail

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen">
 
-      <BasicInput text="SHIPMENT ID" />
+      <BasicInput text="SHIPMENT ID" placeholder="Enter ID..." />
     </h1>
   );
 }
