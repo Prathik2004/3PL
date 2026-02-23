77ccf959757d86a2df74300a89ba2582abb8cedc
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2026-02-23 17:10:42 +0530

    Added userRoutes in dev

--- backend-js/src/index.ts
+++ backend-js/src/index.ts
@@ -5,7 +5,7 @@ import express, { Request, Response, NextFunction } from "express";
 import shipmentRoutes from "./modules/shipment/route"; // Ensure 'Shipment'
 // matches folder name exactly
 import exceptionRoutes from "./modules/Expections/exceptionRoutes";
-
+import userRoutes from "./modules/User/UserRoutes";
 import connectDB from "./config/database";
 import dotenv from "dotenv";
 import runExceptionTracker from "./cron/exceptionTracker";
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 app.use("/api/auth", authRoutes);
-
+app.use("/api/users", userRoutes);
 // Add this above your app.use('/api/shipments', ...)
 app.get("/", (req: Request, res: Response) => {
   res.send("Walkwel 3PL API is running. Use /api/shipments for data.");
