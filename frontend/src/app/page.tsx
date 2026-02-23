9c90588f911ddb6a57f5af3f1ddcbe3fed06aadc
Parthiksen AP <prathiksen.ap@walkweltech.com>
2026-02-23 16:18:54 +0530

    Commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 ">
 
-      <BasicInput text="SHIPMENT ID" />
+      <BasicInput />
     </h1>
   );
 }
