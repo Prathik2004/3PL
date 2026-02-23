cef64d39552cd610dfd5b9a4ada97646e2293850
Jasskaran-Singh-Shangari <jasskaransingh0007@gmail.com>
2026-02-23 16:02:26 +0530

    Minor changes

--- frontend/src/components/ui/BasicButton.tsx
+++ frontend/src/components/ui/BasicButton.tsx
@@ -11,7 +11,7 @@ const BasicButton = ({ text, className = "", ...props }: BasicButtonProps) => {
       className={`flex items-center justify-center transition-all outline-none ${className ? className : 'w-[142px] h-[46px] py-3 px-12 rounded-lg bg-slate-100 hover:bg-slate-200 text-slate-900'}`}
       {...props}
     >
-      <span className="text-[14px] leading-none">{text}</span>
+      <span className="text-[14px] leading-none">{text.toUpperCase()}</span>
     </button>
   )
 }
