8a6827c1537a0de9bc54cdca349afec87d522c70
Jasskaran-Singh-Shangari <jasskaransingh0007@gmail.com>
2026-02-23 15:30:46 +0530

    Delete Modal added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,7 @@ import DeleteModal from "../components/ui/DeleteModal";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 ">
-        <DeleteModal />
+    <h1 className="min-h-screen text-2xl font-semibold text-[#0F172A] p-10 space-y-5 bg-red-500">
     </h1>
   );
 }
