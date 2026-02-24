d8697d9b73fa3ca92cbd195feac5d8a5f0a4d82f
RaHuL79051 <rahulsaxena23012004@gmail.com>
2026-02-24 12:05:34 +0530

    Updated

--- backend-php/app/Modules/Shipment/Validators/CreateShipmentValidator.php
+++ backend-php/app/Modules/Shipment/Validators/CreateShipmentValidator.php
@@ -14,8 +14,8 @@ class CreateShipmentValidator
             'client_name'            => 'required|string|max:255',
             'origin'                 => 'required|string|max:255',
             'destination'            => 'required|string|max:255',
-            'dispatch_date'          => 'required|date',
-            'expected_delivery_date' => 'required|date|after:dispatch_date',
+            'dispatch_date'          => 'required|date_format:Y-m-d',
+            'expected_delivery_date' => 'required|date_format:Y-m-d|after_or_equal:dispatch_date',
             'carrier_name'           => 'required|string|max:255',
             'pod_received'           => 'sometimes|boolean',
         ]);
