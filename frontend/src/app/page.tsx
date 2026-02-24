a79b5931079c684dcdbd1ca7b372060d6dc4b244
RaHuL79051 <rahulsaxena23012004@gmail.com>
2026-02-24 09:44:09 +0530

    FOrget Password

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 "use client"
 import { useState } from "react";
+=======
+<<<<<<< HEAD
+import ExportButton from "../components/ui/ExportButton";
+import NewShipmentButton from "../components/ui/NewShipmentButton";
+
+export default function Home() {
+  return (
+    <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5">
+        
+=======
+>>>>>>> bbd0ccaf3e2849c525a15a916a2723cca6ef0424
 import BasicInput from "../components/ui/BasicInput";
 import BulkUploadModal from "../components/ui/BulkUploadModal";
 import DeleteModal from "../components/ui/DeleteModal";
@@ -7,10 +19,15 @@ import ExportModal from "../components/ui/ExportModal";
 
 export default function Home() {
 
+<<<<<<< HEAD
   const [openModal, setOpenModal]=useState(true)
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen">
       <ExportModal />
+=======
+      <BasicInput />
+>>>>>>> 189f030a88f25e69b0488e69f314441e67b861e4
+>>>>>>> bbd0ccaf3e2849c525a15a916a2723cca6ef0424
     </h1>
   );
 }
