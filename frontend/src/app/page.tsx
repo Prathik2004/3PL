b250931ab097602c30fb04026fbc8c595a1c5beb
Jasskaran-Singh-Shangari <jasskaransingh0007@gmail.com>
2026-02-24 11:21:28 +0530

    Reset password components made and route added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
+import ResetPasswordCard from "../components/ResetPasswordCard";
 
 
 export default function Home() {
 
   return (
-    <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen flex items-center justify-center">
-
+    <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen flex items-center ">
+      <ResetPasswordCard />
     </h1>
   );
 }
--- frontend/src/components/ui/login/LoginCard.tsx
+++ frontend/src/components/ui/login/LoginCard.tsx
@@ -16,7 +16,7 @@ const LoginCard = () => {
     }
 
   return (
-    <div className='shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] p-8 border border-[#E2E8F0] rounded-xl w-[30%] flex flex-col'>
+    <div className='shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] p-8 border border-[#E2E8F0] rounded-xl flex flex-col'>
         {/* HEADING */}
       <div className='flex flex-col text-left mb-10'>
         <span className='text-[22px]/[28px] font-bold'>Welcome back</span>
