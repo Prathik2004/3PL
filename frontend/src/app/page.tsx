e8a425b7c2a69cd773c09827fd036d0d25483b80
abhiobourne <jatinjadonjj@gmail.com>
2026-02-24 13:07:37 +0530

    UserManagement cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,12 @@
 import ResetPasswordCard from "../components/ResetPasswordCard";
+import InviteUserCard from "../components/userManagement/InviteUserCard";
 
 
 export default function Home() {
 
-<<<<<<< HEAD
   return (
     <h1 className="text-2xl font-semibold text-[#0F172A] p-10 space-y-5 min-h-screen flex items-center ">
-      <ResetPasswordCard />
-=======
-      <BasicInput />
->>>>>>> 189f030a88f25e69b0488e69f314441e67b861e4
->>>>>>> bbd0ccaf3e2849c525a15a916a2723cca6ef0424
+     <InviteUserCard />
     </h1>
   );
 }
